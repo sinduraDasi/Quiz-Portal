72876485c05aff2d58ef2ac3672d120bdb44b3dd
sindu1828 <84931658+sinduraDasi@users.noreply.github.com>
2022-11-09 19:41:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Test your skill</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
+        <link rel="shortcut icon" type="image/png" href="quiz.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/bg.jpg) ;
+                
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+
+
+    </head>
+        <center>
+            <div class="intro">
+                <h1> online quiz system </h1>
+                <a href="log.php" class="btn"> login </a> &emsp;
+                <a href="home.php" class="btn"> register </a>
+                <h2> Good &nbsp;Luck. </h2>
+            </div>
+        </center>
+        
+        
+
+        </body>
+</html>
---
+++ log.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Login | Online Quiz System</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/ionicons.min.css">
+		<link rel="stylesheet" href="css/form.css">
+        <style type="text/css">
+            body{
+                  width: 100%;
+                  background: url(image/bg.jpg) ;
+                  background-position: center center;
+                  background-repeat: no-repeat;
+                  background-attachment: fixed;
+                  background-size: cover;
+                }
+          </style>
+	</head>
+
+	<body>
+	<button type="button" class="close" ><a href="http://localhost/ekta/index.php">&times;</a></button>
+		<section class="login first grey">
+			<div class="container">
+				<div class="box-wrapper">				
+					<div class="box box-border">
+						<div class="box-body">
+						<center> <h5 style="font-family: Noto Sans;">Login to </h5><h4 style="font-family: Noto Sans;">Online Quiz System</h4></center><br>
+							<form method="post" action="login.php?q=index.php" >
+								<div class="form-group">
+									<label>Enter Your Email Id:</label>
+									<input type="email" name="email" class="form-control">
+								</div>
+								<div class="form-group">
+									<label class="fw">Enter Your Password:
+										
+									</label>
+									<input type="password" name="password" class="form-control">
+								</div> 
+								<div class="form-group text-right">
+									<button class="btn btn-primary btn-block" name="submit">Login</button>
+									
+								</div>
+								<div class="form-group text-center">
+									<span class="text-muted">Don't have an account?</span> <a href="home.php">Register</a> Here..
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<script src="js/jquery.js"></script>
+		<script src="scripts/bootstrap/bootstrap.min.js"></script>
+	</body>
+</html>
